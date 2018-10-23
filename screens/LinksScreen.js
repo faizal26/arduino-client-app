f5175ce9d66b191c507ca098fbaf4c014e574bd6
Zharif Othman <zharifothman92@gmail.com>
2018-10-23 14:54:00 +0800

    update screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, Image, } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+
+import { Card, Button, Icon } from 'react-native-elements';
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    header: null,
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 15,
+          marginBottom: 15
+          }}>
+          <Image
+            style={{width: 200, height: 200}} 
+            source={require('../assets/images/right.png')}/>
+        </View>
+
+        <Card title='Current Engine Temperature'>        
+          <Text
+            style={{marginTop: 20, marginBottom: 20, textAlign: 'center'}}
+            image={require('../assets/images/right.png')}>
+            Engine Temperature: 16° Celcius
+            {'\n'}Recorded Time: 22/10/2018
+          </Text>
+          <Button
+            icon={<Icon name='code' color='#ffffff' />}
+            backgroundColor='#03A9F4'
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 10}}
+            title='REFRESH STATUS' />
+        </Card>
       </ScrollView>
     );
   }
--- screens/TemperatureScreen.js
+++ screens/TemperatureScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, Text, Image, ActivityIndicator } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
-import { Card, Button, Icon } from 'react-native-elements';
 
 import Temperature from './../components/Temperature';
 
@@ -52,36 +51,7 @@ export default class SettingsScreen extends React.Component {
 
      return (
       <ScrollView style={styles.container}>
-
-        {/* <Card title='All Fine'>
-          <View style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 15,
-            marginBottom: 15
-            }}>
-            <Image
-              style={{width: 200, height: 200}} 
-              source={require('../assets/images/right.png')}/>
-          </View>
-          
-          <Text
-            style={{marginBottom: 10, textAlign: 'center'}}
-            image={require('../assets/images/right.png')}>
-            Temperature value should be here: 16 Celcius
-          </Text>
-          <Button
-            icon={<Icon name='code' color='#ffffff' />}
-            backgroundColor='#03A9F4'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW' />
-        </Card> */}
-
-        {
-          dataTemperatures
-        }
-       
-
+        { dataTemperatures }
       </ScrollView>
     );
   }
