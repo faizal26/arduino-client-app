231ee1a9eeef3db4f1ea85ccf794613a31dc76ba
Zharif Othman <zharifothman92@gmail.com>
2018-12-27 00:00:24 +0800

    bind data with graph

--- screens/AnalyticScreen.js
+++ screens/AnalyticScreen.js
@@ -22,22 +22,6 @@ const height = 200;
 const { width } = Dimensions.get('window');
 const verticalPadding = 5;
 
-const data = [
-  { x: new Date(2018, 9, 1), y: 0 },
-  { x: new Date(2018, 9, 16), y: 0 },
-  { x: new Date(2018, 9, 17), y: 200 },
-  { x: new Date(2018, 10, 1), y: 200 },
-  { x: new Date(2018, 10, 2), y: 300 },
-  { x: new Date(2018, 10, 5), y: 300 },
-];
-
-const scaleX = scaleTime().domain([new Date(2018, 9, 1), new Date(2018, 10, 5)]).range([0, width]);
-const scaleY = scaleLinear().domain([0, 300]).range([height - verticalPadding, verticalPadding])
-const line = d3.shape.line()
- .x(d => scaleX(d.x))
- .y(d => scaleY(d.y))
- .curve(d3.shape.curveBasis)(data);
-
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'Temperature Analytics',
@@ -50,56 +34,110 @@ export default class SettingsScreen extends React.Component {
       data: null,
       minDate: null,
       maxDate: null,
+      newData: null,
+      maxTemperature: null,
+      line: null,
     }
   }
 
-  componentDidMount() {
-    return fetch('https://arduino-server-api.firebaseapp.com/api/temperatures')
-      .then((res) => res.json())
-      .then((results) => {
-        this.setState({
-          isLoading: false,
-          data: results,
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+  async componentDidMount() {
+    const response = await fetch('https://arduino-server-api.firebaseapp.com/api/temperatures');
+    const json = await response.json();
+
+    this.setState({ 
+      data: json,
+    })
+    
+    return this.manageData();
   }
 
-  // convertTime(array) {
-  //   const newDate = []
-  //   if (array !== null) {
-  //     array.map((item) => {
-  //       newDate.push(new Date(item.timestamp))
-  //     })
-  //   }
+  async assignData() {
+    const data = this.state.newData;
+    const minimumDate = this.state.newData[0].x;
+    const maximumDate = this.state.newData[9].x; 
+    const maximumTemperature = this.state.maxTemperature;
 
-  //   const maxDate = new Date(Math.max.apply(null, newDate));
-  //   const minDate = new Date(Math.max.apply(null, newDate));
+    const scaleX = scaleTime().domain([minimumDate, maximumDate]).range([0, width]);
+    const scaleY = scaleLinear().domain([0, maximumTemperature]).range([height - verticalPadding, verticalPadding])
+    const line = d3.shape.line()
+      .x(d => scaleX(d.x))
+      .y(d => scaleY(d.y))
+      .curve(d3.shape.curveBasis)(data);
+    this.setState({
+      line,
+      isLoading: false,
+    })
+  }
 
-  //   this.setState({ maxDate, minDate });
-  // }
+  async manageData() {
+    const dataArray = this.state.data;
+
+    const formattedData = await dataArray.map((dataObject) => {
+      const newDate = this.getTime(dataObject.timestamp);
+      const formattedData = {
+        x: newDate,
+        y: dataObject.temperature,
+      }
+      return formattedData;
+    })
+
+    const temperatureData = await dataArray.map((dataObject) => {
+      return dataObject.temperature;
+    })
+
+    const maxTemperature = await Math.max(...temperatureData);
+    this.setState({
+      maxTemperature,
+    })
+
+    this.setState({
+      newData: formattedData,
+      maxTemperature,
+    });
+
+    return this.assignData();
+  }
+
+  getTime(unix_timestamp) {
+    var date = new Date(unix_timestamp);
+    var seconds = "0" + date.getSeconds();
+    var minutes = "0" + date.getMinutes();
+    var hours = date.getHours();
+    var day = date.getDate();
+    var month = date.getMonth() + 1; // add 1 because getMonth() is based zero function
+    var year = date.getFullYear();
+
+    var formattedTime = new Date(year, month, day, hours, minutes, seconds);
+    return formattedTime;
+  }
 
   render() {
-    //  this.convertTime(this.state.data);
-
-     return (
-      <View style={styles.container}>
-        <View style={styles.analyticBox}>
-          <Svg style={{ width, height }}>
-            <Defs>
-              <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="gradient">
-                <Stop stopColor="#FF1A1A" offset="0%" />
-                <Stop stopColor="#FEFFFF" offset="100%" />
-              </LinearGradient>
-            </Defs>
-            <Path d={line} fill="transparent" stroke="red" strokeWidth={2} />
-            <Path d={`${line} L ${width} ${height} L 0 ${height}`} fill="url(#gradient)" />
-          </Svg>
+    if(this.state.isLoading) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#0000ff" />
         </View>
-      </View>
-    );
+      )
+    } else {
+      // console.log('max temperature:', this.state.maxTemperature);
+      // console.log('new data:', this.state.newData);
+      return (
+        <View style={styles.container}>
+          <View style={styles.analyticBox}>
+            <Svg style={{ width, height }}>
+              <Defs>
+                <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="gradient">
+                  <Stop stopColor="#FF1A1A" offset="0%" />
+                  <Stop stopColor="#FEFFFF" offset="100%" />
+                </LinearGradient>
+              </Defs>
+              <Path d={this.state.line} fill="transparent" stroke="red" strokeWidth={2} />
+              <Path d={`${this.state.line} L ${width} ${height} L 0 ${height}`} fill="url(#gradient)" />
+            </Svg>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
@@ -108,6 +146,11 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
   },
   analyticBox: {
     marginTop: 60,
